1f4d9e0d0b520355fc9e288bffd5a2ff82128d84
Robin Rathore <robinsingh248142@gmail.com>
2024-05-13 20:19:02 +0530

    postcss added

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ],
-  };
+export default {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
   
